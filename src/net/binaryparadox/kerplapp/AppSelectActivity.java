1a971cb3409906fdd2fd780b07ac7e537e5d304a
Hans-Christoph Steiner <hans@eds.org>
2013-11-26 14:45:56 -0500

    rename menu files after the classes that they are attached to

--- src/net/binaryparadox/kerplapp/AppSelectActivity.java
+++ src/net/binaryparadox/kerplapp/AppSelectActivity.java
@@ -36,7 +36,7 @@ public class AppSelectActivity extends FragmentActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.app_select, menu);
+        getMenuInflater().inflate(R.menu.app_select_activity, menu);
         return true;
     }
 
